dc3cbe093fadfe5ce9b2b266355dc73ff0d9a002
arianaaghajani <aghajaniariana@gmail.com>
2024-03-13 16:00:35 +0330

    show task field

--- src/main/java/bash.sh
+++ src/main/java/bash.sh
@@ -6,3 +6,22 @@ deleted_file="deleted_task.txt"
 touch $done_file
 touch $todo_file
 touch $deleted_file
+
+show_tasks(){
+  status=$1
+  case $status in
+   "todo")
+   echo "tasks to do:"
+   cat $todo_file
+    ;;
+  "done")
+  echo "tasks done:"
+  cat $done_file
+    ;;
+  "deleted")
+  echo "tasks deleted"
+  cat deleted_file
+  ;;
+  esac
+
+}
